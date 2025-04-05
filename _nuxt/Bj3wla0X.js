ff3ecbd04140d523d876c987176111e9c98f4683
T4kano <edutakajr@gmail.com>
2025-04-05 19:56:55 -0300

    rebuild

---
+++ _nuxt/Bj3wla0X.js
@@ -0,0 +1,1 @@
+import"./CuN2yQEi.js";const t=""+new URL("brand.BvelbFXZ.png",import.meta.url).href;export{t as _};
---
+++ _nuxt/C-UsCGRc.js
@@ -0,0 +1,1 @@
+import{c as e,o as c}from"./CuN2yQEi.js";const r={__name:"schedule",setup(t){return(a,n)=>(c(),e("h1",null,"Agenda"))}};export{r as default};
